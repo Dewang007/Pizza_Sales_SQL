fb10f79913e8288f7a82ad43dc0148b5fe7edeea
Dewang007 <151505719+Dewang007@users.noreply.github.com>
2024-04-11 01:08:22 +0530

    Add files via upload

---
+++ Q2.sql
@@ -0,0 +1,9 @@
+-- Calculate the total revenue generated from pizza sales.
+
+SELECT 
+    ROUND(sum(order_details.quantity * pizzas.price),
+            2) AS total_sales
+FROM
+    order_details
+        JOIN
+    pizzas ON pizzas.pizza_id = order_details.pizza_id;
